befe08ab30a237dae45d363a8b3ac79f47c34784
AlexCardenas55 <acardenas55@hotmail.com>
2022-12-09 01:06:37 -0600

    navBar component + dependencies update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MenubarModule} from 'primeng/menubar';
+import { NavbarComponent } from './navbar/navbar.component';
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
+    CardModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
